3fb35e500b2a8a76fd2698c84b326edd83677bf9
wdalmorra <wddsouza@inf.ufpel.edu.br>
2013-03-04 09:59:25 -0300

    Modificações no grafo

--- Grafo.cpp
+++ Grafo.cpp
@@ -2,7 +2,54 @@
 
 Grafo::Grafo(int vertices){
 
-	// Cria o ponteiro da matriz com n vertices
-	matrizAdj = malloc((vertices*vertices)*sizeof(int));
+	int i,j;
 
+	// Cria a tabela de referencias
+	// tabelaRef = (string*) malloc(vertices*sizeof(string));
+
+	// Cria o ponteiro da matriz com n vertices
+	matrizAdj = (int*) malloc((vertices*vertices)*sizeof(int));
+
+	// Inicia a matriz com valor "nulo"
+	for (i = 0; i < vertices; ++i) {
+		for (j = 0; j < vertices; ++j) {
+			matrizAdj[i*vertices + j] = -1;
+		}
+	}
+
+	numeroVert = vertices;
+
+} // Fim construtor
+
+void Grafo::inserirVertice(int ID, string nome)
+{
+	tabelaRef[ID] = nome;
+}// Fim inserirVertice
+
+void Grafo::inserirAresta(int ID1, int ID2, int peso)
+{
+	matrizAdj[ID1*numeroVert+ID2] = peso;
+}
+
+void Grafo::removerVertice(int ID)
+{
+	int j;
+	if (tabelaRef[ID].compare("") == 0)
+	{
+		for (j = 0; j < numeroVert; ++j) {
+
+			// Apaga todas as arestas para o qual ele aponta
+			matrizAdj[ID*numeroVert+j] = -1;
+
+			// Apaga todas as arestas para o qual apontam para ele
+			matrizAdj[j*numeroVert+ID] = -1;
+		}
+	}
+}
+
+void Grafo::imprimeTable()
+{
+	for (int i = 0; i < numeroVert; ++i) {
+		cout<< tabelaRef[i]<< endl;
+	}
 }
--- Grafo.h
+++ Grafo.h
@@ -1,13 +1,26 @@
-#include <cstream>
-#include <cstdlib>
+#ifndef GRAFO_H_
+#define GRAFO_H_
 
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
 class Grafo
 {
 	int *matrizAdj;
+	string tabelaRef[100];
+	int numeroVert;
 
 public:
+
 	Grafo(int vertices);
 	~Grafo();
 
-	/* data */
+	void inserirVertice(int ID, string nome);
+	void inserirAresta(int ID1, int ID2, int peso);
+	void removerVertice(int ID);
+	void imprimeTable();
 };
+
+#endif
